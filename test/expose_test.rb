8668233d20302f0add57ce828945157c76228516
Jake Gordon <jake@codeincomplete.com>
2016-07-16 15:13:30 -0700

    simplify expose_test cases

--- test/expose_test.rb
+++ test/expose_test.rb
@@ -5,40 +5,63 @@ module RubySerializer
 
     #----------------------------------------------------------------------------------------------
 
-    class Serializer < RubySerializer::Base
+    ID     = 42
+    NAME   = 'Name'
+    EMAIL  = 'Email'
+    SECRET = 'Secret'
 
+    Resource = Struct.new(:id, :name, :email, :secret)
+
+    #----------------------------------------------------------------------------------------------
+
+    class BasicSerializer < RubySerializer::Base
       expose :id
       expose :name
-      expose :url, as:   :website              # expose resource attribute AS serialized attribute
-      expose :hq,  from: :headquarters         # expose serialized attribute FROM resource attribute
+      expose :email
+    end
 
-      namespace :financial do                  # group attributes in a namespace
-        expose :ticker
-        namespace :current do                  # group attributes in a nested namespace
-          expose :price
-          expose :cap
-        end
+    class RenamingSerializer < RubySerializer::Base
+      expose :id
+      expose :name,       as:   :user_name
+      expose :user_email, from: :email
+    end
+
+    class NamespaceSerializer < RubySerializer::Base
+      expose :id
+      namespace :user do
+        expose :name
+        expose :email
       end
-
     end
 
     #----------------------------------------------------------------------------------------------
 
-    def test_expose_options
+    def test_expose_attributes_unchanged
+      resource = Resource.new(ID, NAME, EMAIL, SECRET)
+      json = RubySerializer.serialize resource, with: BasicSerializer
+      assert_equal [ :id, :name, :email ], json.keys
+      assert_equal ID,    json[:id]
+      assert_equal NAME,  json[:name]
+      assert_equal EMAIL, json[:email]
+    end
 
-      resource = OpenStruct.new(GOOGLE)
-      json     = RubySerializer.serialize resource, with: Serializer
-      expected = [ :id, :name, :website, :hq, :financial ]
+    def test_expose_renamed_attributes
+      resource = Resource.new(ID, NAME, EMAIL, SECRET)
+      json = RubySerializer.serialize resource, with: RenamingSerializer
+      assert_equal [ :id, :user_name, :user_email ], json.keys
+      assert_equal ID,    json[:id]
+      assert_equal NAME,  json[:user_name]
+      assert_equal EMAIL, json[:user_email]
+    end
 
-      assert_set   expected,              json.keys,                           'expected only known keys to be serialized'
-      assert_equal GOOGLE[:id],           json[:id],                           'expected :id to be exposed as-is'
-      assert_equal GOOGLE[:name],         json[:name],                         'expected :name to be exposed as-is'
-      assert_equal GOOGLE[:url],          json[:website],                      'expected :url (resource attribute) to be exposed AS :website (serialized attribute)'
-      assert_equal GOOGLE[:headquarters], json[:hq],                           'expected :hq (serialized attribute) to be exposed FROM :headquarters (resource attribute)'
-      assert_equal GOOGLE[:ticker],       json[:financial][:ticker],           'expected :ticker to be exposed within the :financial NAMESPACE'
-      assert_equal GOOGLE[:price],        json[:financial][:current][:price],  'expected :price to be exposed within a nested :financial/current NAMESPACE'
-      assert_equal GOOGLE[:cap],          json[:financial][:current][:cap],    'expected :market_cap to be exposed within a nested :financial/current NAMESPACE'
-
+    def test_expose_namespaced_attributes
+      resource = Resource.new(ID, NAME, EMAIL, SECRET)
+      json = RubySerializer.serialize resource, with: NamespaceSerializer
+      assert_equal [ :id, :user ],    json.keys
+      assert_equal ID,                json[:id]
+      assert_equal NAME,              json[:user][:name]
+      assert_equal EMAIL,             json[:user][:email]
+      assert_equal [ :name, :email ], json[:user].keys
     end
 
     #----------------------------------------------------------------------------------------------
