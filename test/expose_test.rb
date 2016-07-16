02e72dc1f7eaeebecff90fdc92499855516ab67b
Jake Gordon <jake@codeincomplete.com>
2016-07-16 14:15:59 -0700

    added unit test for #expose with namespace

--- test/expose_test.rb
+++ test/expose_test.rb
@@ -6,9 +6,19 @@ module RubySerializer
     #----------------------------------------------------------------------------------------------
 
     class ExposeTestSerializer < RubySerializer::Base
+
       expose :id
       expose :name
       expose :url, as: :website
+
+      namespace :financial do
+        expose :ticker
+        namespace :current do
+          expose :price
+          expose :market_cap, as: :cap
+        end
+      end
+
     end
 
     #----------------------------------------------------------------------------------------------
@@ -17,12 +27,15 @@ module RubySerializer
 
       resource = OpenStruct.new(GOOGLE)
       json     = RubySerializer.serialize resource, with: ExposeTestSerializer
-      expected = [ :id, :name, :website ]
+      expected = [ :id, :name, :website, :financial ]
 
-      assert_set   expected,      json.keys,      'expected only known keys to be serialized'
-      assert_equal GOOGLE[:id],   json[:id],      'expected :id to be exposed as-is'
-      assert_equal GOOGLE[:name], json[:name],    'expected :name to be exposed as-is'
-      assert_equal GOOGLE[:url],  json[:website], 'expected :url to be exposed as :website'
+      assert_set   expected,            json.keys,                           'expected only known keys to be serialized'
+      assert_equal GOOGLE[:id],         json[:id],                           'expected :id to be exposed as-is'
+      assert_equal GOOGLE[:name],       json[:name],                         'expected :name to be exposed as-is'
+      assert_equal GOOGLE[:url],        json[:website],                      'expected :url to be exposed AS :website'
+      assert_equal GOOGLE[:ticker],     json[:financial][:ticker],           'expected :ticker to be exposed within the :financial NAMESPACE'
+      assert_equal GOOGLE[:price],      json[:financial][:current][:price],  'expected :price to be exposed within a nested :financial/current NAMESPACE'
+      assert_equal GOOGLE[:market_cap], json[:financial][:current][:cap],    'expected :market_cap to be exposed within a nested :financial/current NAMESPACE AS :cap'
 
     end
 
