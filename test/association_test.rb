9422c290ba727b0258eaf71d96aa2115e7e654e0
Jake Gordon <jake@codeincomplete.com>
2016-07-17 12:59:13 -0700

    unit test to verify model can be serialized with multiple :include(s)

--- test/association_test.rb
+++ test/association_test.rb
@@ -5,29 +5,78 @@ module RubySerializer
 
     #----------------------------------------------------------------------------------------------
 
-    AW = { id: 123, name: 'Addison-Wesley' }
-    MS = { id: 99,  name: 'Microsoft Press' }
-    PP = { isbn: '020161622X', name: 'Pragmatic Programmer', publisher_id: AW[:id] }
-    DP = { isbn: '0201633612', name: 'Design Patterns',      publisher_id: AW[:id] }
-    CC = { isbn: '0735619670', name: 'Code Complete',        publisher_id: MS[:id] }
+    PORTLAND = { id: :portland, city: 'Portland',      state: 'OR' }
+    SEATTLE  = { id: :seattle,  city: 'Seattle',       state: 'WA' }
+    BOSTON   = { id: :boston,   city: 'Boston',        state: 'MA' }
+    NYC      = { id: :nyc,      city: 'New York',      state: 'NY' }
+    SF       = { id: :sf,       city: 'San Francisco', state: 'CA' }
+    LA       = { id: :la,       city: 'Los Angeles',   state: 'LA' }
+    CHICAGO  = { id: :chicago,  city: 'Chicago',       state: 'IL' }
+    DALLAS   = { id: :dallas,   city: 'Dallas',        state: 'TX' }
+    REDMOND  = { id: :redmond,  city: 'Redmond',       state: 'WA' }
 
-    PORTLAND = { city: 'Portland', state: 'OR' }
+    MCCONNEL  = { id: :mcconnel,  name: 'Steve McConnel', address: SEATTLE  }
+    HUNT      = { id: :hunt,      name: 'Andrew Hunt',    address: PORTLAND }
+    THOMAS    = { id: :thomas,    name: 'David Thomas',   address: BOSTON   }
+    GAMMA     = { id: :gamma,     name: 'Erich Gamma',    address: NYC      }
+    HELM      = { id: :helm,      name: 'Richard Helm',   address: SF       }
+    JOHNSON   = { id: :johnson,   name: 'Ralph Johnson',  address: CHICAGO  }
+    VLISSIDES = { id: :vlissides, name: 'John Vlissides', address: DALLAS   }
+
+    PP = { id: :pp, isbn: '020161622X', name: 'Pragmatic Programmer', publisher_id: :aw, authors: [ HUNT, THOMAS ] }
+    DP = { id: :dp, isbn: '0201633612', name: 'Design Patterns',      publisher_id: :aw, authors: [ GAMMA, HELM, JOHNSON, VLISSIDES ] }
+    CC = { id: :cc, isbn: '0735619670', name: 'Code Complete',        publisher_id: :ms, authors: [ MCCONNEL ] }
+
+    AW = { id: :aw, name: 'Addison-Wesley',  address: LA,      books: [ PP, DP ]  }
+    MS = { id: :ms, name: 'Microsoft Press', address: REDMOND, books: [ CC ]      }
+
+    #----------------------------------------------------------------------------------------------
+
+    def addresses(fixture)
+      Address.new(fixture)
+    end
+
+    def authors(fixture)
+      author = Author.new(fixture)
+      author.address = addresses(fixture[:address]) if fixture.key?(:address)
+      author
+    end
+
+    def books(fixture, options = {})
+      book = Book.new(fixture)
+      book.publisher = publishers(options[:publisher]) if options.key?(:publisher)
+      book.authors   = Array(fixture[:authors]).map { |a| authors(a) } if fixture.key?(:authors)
+      book
+    end
+
+    def publishers(fixture)
+      publisher = Publisher.new(fixture)
+      publisher.address = addresses(fixture[:address]) if fixture.key?(:address)
+      publisher.books = Array(fixture[:books]).map { |b| books(b) } if fixture.key?(:books)
+      publisher
+    end
 
     #----------------------------------------------------------------------------------------------
 
     class Publisher < Model
       attr :id, :name
+      has_one  :address
       has_many :books
-      has_one :address
     end
 
     class Book < Model
       attr :isbn, :name
       belongs_to :publisher
+      has_many :authors
+    end
+
+    class Author < Model
+      attr :id, :name
+      has_one :address
     end
 
     class Address < Model
-      attr :city, :state
+      attr :id, :city, :state
     end
 
     #----------------------------------------------------------------------------------------------
@@ -35,8 +84,8 @@ module RubySerializer
     class PublisherSerializer < RubySerializer::Base
       expose   :id
       expose   :name
-      has_many :books
       has_one  :address
+      has_many :books
     end
 
     class BookSerializer < RubySerializer::Base
@@ -45,7 +94,14 @@ module RubySerializer
       belongs_to :publisher
     end
 
+    class AuthorSerializer < RubySerializer::Base
+      expose   :id
+      expose   :name
+      has_one  :address
+    end
+
     class AddressSerializer < RubySerializer::Base
+      expose :id
       expose :city
       expose :state
     end
@@ -53,26 +109,22 @@ module RubySerializer
     #----------------------------------------------------------------------------------------------
 
     def test_belongs_to
-      publisher      = Publisher.new(AW)
-      book           = Book.new(PP)
-      book.publisher = publisher
-      json           = serialize book, include: :publisher
-      expected       = [ :isbn, :name, :publisher_id, :publisher ]
-      assert_set   expected,        json.keys
-      assert_equal PP[:isbn],       json[:isbn]
-      assert_equal PP[:name],       json[:name]
-      assert_equal AW[:id],         json[:publisher_id]
-      assert_equal [ :id, :name ],  json[:publisher].keys
-      assert_equal AW[:id],         json[:publisher][:id]
-      assert_equal AW[:name],       json[:publisher][:name]
+      book     = books(PP, publisher: AW)
+      json     = serialize book, include: :publisher
+      expected = [ :isbn, :name, :publisher_id, :publisher ]
+      assert_set   expected,       json.keys
+      assert_equal PP[:isbn],      json[:isbn]
+      assert_equal PP[:name],      json[:name]
+      assert_equal AW[:id],        json[:publisher_id]
+      assert_equal [ :id, :name ], json[:publisher].keys
+      assert_equal AW[:id],        json[:publisher][:id]
+      assert_equal AW[:name],      json[:publisher][:name]
     end
 
     def test_belongs_to_but_not_included
-      publisher      = Publisher.new(AW)
-      book           = Book.new(PP)
-      book.publisher = publisher
-      json           = serialize book
-      expected       = [ :isbn, :name, :publisher_id ]
+      book     = books(PP, publisher: AW)
+      json     = serialize book
+      expected = [ :isbn, :name, :publisher_id ]
       assert_set   expected,  json.keys
       assert_equal PP[:isbn], json[:isbn]
       assert_equal PP[:name], json[:name]
@@ -83,37 +135,34 @@ module RubySerializer
     #----------------------------------------------------------------------------------------------
 
     def test_has_one
-      publisher = Publisher.new(AW)
-      publisher.address = Address.new(PORTLAND)
-      json     = serialize publisher, include: :address
+      author   = authors(HUNT)
+      json     = serialize author, include: :address
       expected = [ :id, :name, :address ]
-      assert_set   expected,          json.keys
-      assert_equal AW[:id],           json[:id]
-      assert_equal AW[:name],         json[:name]
-      assert_equal [ :city, :state ], json[:address].keys
-      assert_equal PORTLAND[:city],   json[:address][:city]
-      assert_equal PORTLAND[:state],  json[:address][:state]
+      assert_set   expected,               json.keys
+      assert_equal HUNT[:id],              json[:id]
+      assert_equal HUNT[:name],            json[:name]
+      assert_equal [ :id, :city, :state ], json[:address].keys
+      assert_equal PORTLAND[:id],          json[:address][:id]
+      assert_equal PORTLAND[:city],        json[:address][:city]
+      assert_equal PORTLAND[:state],       json[:address][:state]
     end
 
     def test_has_one_but_not_included
-      publisher = Publisher.new(AW)
-      publisher.address = Address.new(PORTLAND)
-      json     = serialize publisher
+      author   = authors(HUNT)
+      json     = serialize author
       expected = [ :id, :name ]
-      assert_set   expected,  json.keys
-      assert_equal AW[:id],   json[:id]
-      assert_equal AW[:name], json[:name]
-      assert_equal nil,       json[:address]
+      assert_set   expected,    json.keys
+      assert_equal HUNT[:id],   json[:id]
+      assert_equal HUNT[:name], json[:name]
+      assert_equal nil,         json[:address]
     end
 
     #----------------------------------------------------------------------------------------------
 
     def test_has_many
-      publisher = Publisher.new(AW)
-      publisher.books << Book.new(PP)
-      publisher.books << Book.new(DP)
-      json     = serialize publisher, include: :books
-      expected = [ :id, :name, :books ]
+      publisher = publishers(AW)
+      json      = serialize publisher, include: :books
+      expected  = [ :id, :name, :books ]
       assert_set   expected,                        json.keys
       assert_equal AW[:id],                         json[:id]
       assert_equal AW[:name],                       json[:name]
@@ -129,11 +178,9 @@ module RubySerializer
     end
 
     def test_has_many_but_not_included
-      publisher = Publisher.new(AW)
-      publisher.books << Book.new(PP)
-      publisher.books << Book.new(DP)
-      json     = serialize publisher
-      expected = [ :id, :name ]
+      publisher = publishers(AW)
+      json      = serialize publisher
+      expected  = [ :id, :name ]
       assert_set   expected,         json.keys
       assert_equal AW[:id],          json[:id]
       assert_equal AW[:name],        json[:name]
@@ -141,6 +188,32 @@ module RubySerializer
     end
 
     #----------------------------------------------------------------------------------------------
+
+    def test_multiple_includes
+      publisher = publishers(AW)
+      json      = serialize publisher, include: [ :books, :address ]
+      expected  = [ :id, :name, :address, :books ]
+      assert_set expected, json.keys
+      assert_equal AW[:id],                         json[:id]
+      assert_equal AW[:name],                       json[:name]
+      assert_equal [ :id, :city, :state ],          json[:address].keys
+      assert_equal AW[:address][:id],               json[:address][:id]
+      assert_equal AW[:address][:city],             json[:address][:city]
+      assert_equal AW[:address][:state],            json[:address][:state]
+      assert_equal 2,                               json[:books].length
+      assert_equal [ :isbn, :name, :publisher_id ], json[:books][0].keys
+      assert_equal PP[:isbn],                       json[:books][0][:isbn]
+      assert_equal PP[:name],                       json[:books][0][:name]
+      assert_equal AW[:id],                         json[:books][0][:publisher_id]
+      assert_equal [ :isbn, :name, :publisher_id ], json[:books][1].keys
+      assert_equal DP[:isbn],                       json[:books][1][:isbn]
+      assert_equal DP[:name],                       json[:books][1][:name]
+      assert_equal AW[:id],                         json[:books][1][:publisher_id]
+    end
+
+    #----------------------------------------------------------------------------------------------
+
+    private
 
   end
 end
